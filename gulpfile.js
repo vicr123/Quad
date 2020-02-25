8cc88dcae50478c3868fd09bdc905f236146b2e7
Victor Tran <vicr12345@gmail.com>
2020-02-25 22:20:45 +1100

    Use gulp to build quadweb

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,24 @@
 const gulp = require('gulp');
 const fs = require("fs");
 const i18nextParser = require('i18next-parser').gulp;
+const webpack = require('webpack-stream');
 
 let availableTranslations = fs.readdirSync("quad/translations");
 
 gulp.task('i18next', function() {
-    return gulp.src('quad/**/*.js').pipe(new i18nextParser({
-        locales: availableTranslations,
-        output: 'quad/translations/$LOCALE/$NAMESPACE.json',
-        defaultNamespace: 'translation',
-        keySeparator: false,
-        namespaceSeparator: false
-    })).pipe(gulp.dest('./'));
+    return gulp.src('quad/**/*.js')
+        .pipe(new i18nextParser({
+            locales: availableTranslations,
+            output: 'quad/translations/$LOCALE/$NAMESPACE.json',
+            defaultNamespace: 'translation',
+            keySeparator: false,
+            namespaceSeparator: false
+        }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('quadweb', function() {
+    return gulp.src('quadweb/src/index.jsx')
+        .pipe(webpack(require('./quadweb/webpack.config.js')))
+        .pipe(gulp.dest('quadweb/dist'))
 });
